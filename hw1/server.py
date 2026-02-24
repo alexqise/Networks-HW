16c182be9eedb786cb0378c523db9a35f028a502
Alex <alexqi321@gmail.com>
2026-02-24 17:14:13 -0500

    server finished (using demo tcp skeleton code)

--- hw1/server.py
+++ hw1/server.py
@@ -5,3 +5,35 @@
 # server.py - the server program for taking request from the client and 
 #             send the requested file back to the client
 #
+
+import socket
+
+serverport = 60000
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind to port num
+serverSocket.bind(('', serverport))
+
+# server begins listening for incoming TCP requests
+serverSocket.listen(1)
+print("Server is ready to receive connections")
+
+while True:
+    # server waits on accept() for incoming requests, new socket created on return
+    connectionSocket, addr = serverSocket.accept()
+    print(f"Connected by {addr}")
+
+    # read bytes from connection
+    filename = connectionSocket.recv(1024).decode()
+    print(f"Requesting file: {filename}")
+
+    # open the file name that has been requested
+    file = open(filename, 'rb')
+
+    # send it over
+    connectionSocket.sendall(file.read())
+
+    # close
+    file.close()
+    connectionSocket.close()
